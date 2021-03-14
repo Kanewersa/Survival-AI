5417c5e3fb0376a33732f3ac2eeaab86071f74f8
= <kanewers@gmail.com>
2021-03-14 22:57:18 +0100

    Make tiles background random

--- survival/tile.py
+++ survival/tile.py
@@ -1,4 +1,9 @@
+import random
+
+
 class Tile:
+    origins = [(0, 0), (32, 0), (64, 0), (96, 0)]
+
     def __init__(self, origin=(0, 0)):
-        self.origin = origin
+        self.origin = random.choice(Tile.origins)
         self.image = None
