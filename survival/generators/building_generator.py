f449cec09098f319a8746c62e7126c6cf000b958
LukeSkiiWalker <58610760+LukeSkiiWalker@users.noreply.github.com>
2021-05-22 18:32:36 +0200

    BuildingGenerator creates house with 2 chests.

--- survival/generators/building_generator.py
+++ survival/generators/building_generator.py
@@ -5,14 +5,25 @@ from survival.components.sprite_component import SpriteComponent
 
 
 class BuildingGenerator:
-    def create_home(self, world, game_map):
+    def create_home(self, world, game_map, position = [10,10]):
         home = world.create_entity()
-        pos = PositionComponent([32, 32], [32, 32])
-        world.add_component(home, pos)
+        home_pos = PositionComponent([position[0]*32, position[1]*32], position)
+        world.add_component(home, home_pos)
+        game_map.add_entity(home, home_pos)
         world.add_component(home, InventoryComponent())
-
-        game_map.add_entity(home, pos)
-        sprite = SpriteComponent('tree.png')
-        sprite.set_scale(2)
+        sprite = SpriteComponent('home.png')
         world.add_component(home, sprite)
         world.add_component(home, CollisionComponent())
+
+        for x_pos in [-1, 1]:
+            chest = world.create_entity()
+            chest_pos = PositionComponent(
+                [(position[0]+x_pos)*32, position[1]*32],
+                [position[0]+x_pos, position[1]]
+            )
+            world.add_component(chest, chest_pos)
+            game_map.add_entity(chest, chest_pos)
+            world.add_component(chest, InventoryComponent())
+            sprite = SpriteComponent('chest.png')
+            world.add_component(chest, sprite)
+            world.add_component(chest, CollisionComponent())
